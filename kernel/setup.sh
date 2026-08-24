eca38cfa48ccb9af4153a0935c731118d06df762
maxsteeel <109047395+maxsteeel@users.noreply.github.com>
2026-08-24 08:19:41 -0400

    kernel/setup.sh: add support to common/ kernels

--- kernel/setup.sh
+++ kernel/setup.sh
@@ -16,6 +16,8 @@ display_usage() {
 initialize_variables() {
     if [ -d "$GKI_ROOT/fs" ]; then
          FS_DIR="$GKI_ROOT/fs"
+    else if [ -d "$GKI_ROOT/common/fs" ]; then
+         FS_DIR="$GKI_ROOT/common/fs"
     else
          echo '[ERROR] "fs/" directory not found. Are you at the root of the kernel tree?'
          exit 127
